f42d45783ab5519e90e8bdfec575216e08321568
Dan Fuhr <165685619+fuhrdan@users.noreply.github.com>
2026-02-14 08:53:35 -0700

    Updated program with saved code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//*****************************************************************************
+//** 74. Search a 2D Matrix                                         leetcode **
+//*****************************************************************************
+//** We treat the grid as one long, sorted sea,
+//** Binary search sails through it carefully;
+//** With mid divided, row and column found,
+//** In log-time depth the hidden truth is crowned.
+//*****************************************************************************
+
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    if (matrixSize == 0 || matrixColSize[0] == 0)
+    {
+        return false;
+    }
+
+    int m = matrixSize;
+    int n = matrixColSize[0];
+
+    int low = 0;
+    int high = (m * n) - 1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int midElement = matrix[mid / n][mid % n];
+
+        if (midElement == target)
+        {
+            return true;
+        }
+        else if (midElement > target)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    return false;
+}
